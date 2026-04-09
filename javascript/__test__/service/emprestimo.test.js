467d4db328e4d3c8113c2936154db9dc9cdc686c
Lucas Ribeiro <82158676+Luqui05@users.noreply.github.com>
2026-04-09 20:56:56 -0300

    feat: aula 09/04/2026

--- javascript/__test__/service/emprestimo.test.js
+++ javascript/__test__/service/emprestimo.test.js
@@ -1,15 +1,106 @@
-const { Livro } = require("../../src/model/Livro");
+﻿const { Livro } = require("../../src/model/Livro");
 const { Usuario } = require("../../src/model/Usuario");
 const { EmprestimoService } = require("../../src/service/EmprestimoService");
+const { constantes } = require("../../src/utils/constants");
+const casos = require("../data/emprestimo.json");
 
-test("Testa usuario e livro valido", () => {
-    // Arrange
-    const usuario = new Usuario({ id: 1, nome: "Teste", ativo: true, emprestimosAtivos: 1, multaPendente: 5 });
-    const livro = new Livro({ id: 1, titulo: "Chapeuzinho Vermelho", disponivel: true });
+describe("Emprestimo", () => {
+    test("Testa usuario e livro valido", () => {
+        const usuario = new Usuario({
+            id: 1,
+            nome: "Teste",
+            ativo: true,
+            emprestimosAtivos: 1,
+            multaPendente: 5,
+        });
+        const livro = new Livro({
+            id: 1,
+            titulo: "Chapeuzinho Vermelho",
+            disponivel: true,
+        });
 
-    // Act
-    const saida = EmprestimoService.autorizarEmprestimo(usuario, livro);
+        const saida = EmprestimoService.autorizarEmprestimo(usuario, livro);
 
-    // Assert
-    expect(true).toBe(saida);
+        expect(saida).toBe(true);
+    });
+
+    test("Testa usuario valido e livro invalido", () => {
+        const usuario = new Usuario({
+            id: 1,
+            nome: "Teste",
+            ativo: true,
+            emprestimosAtivos: 1,
+            multaPendente: 5,
+        });
+        const livro = new Livro({
+            id: 1,
+            titulo: "Chapeuzinho Vermelho",
+            disponivel: false,
+        });
+
+        expect(() => EmprestimoService.autorizarEmprestimo(usuario, livro)).toThrow(
+            "Livro indisponível",
+        );
+    });
+
+    test("Testa usuario invalido (emprestimo) e livro valido", () => {
+        const usuario = new Usuario({
+            id: 1,
+            nome: "Teste",
+            ativo: true,
+            emprestimosAtivos: constantes.USUARIO_LIMITE_EMPRESTIMOS + 1,
+            multaPendente: 5,
+        });
+        const livro = new Livro({
+            id: 1,
+            titulo: "Chapeuzinho Vermelho",
+            disponivel: true,
+        });
+
+        const saida = EmprestimoService.autorizarEmprestimo(usuario, livro);
+
+        expect(saida).toBe(false);
+    });
+
+    test("Testa usuario invalido (multa) e livro valido", () => {
+        const usuario = new Usuario({
+            id: 1,
+            nome: "Teste",
+            ativo: true,
+            emprestimosAtivos: 0,
+            multaPendente: constantes.USUARIO_LIMITE_MULTA + 1,
+        });
+        const livro = new Livro({
+            id: 1,
+            titulo: "Chapeuzinho Vermelho",
+            disponivel: true,
+        });
+
+        const saida = EmprestimoService.autorizarEmprestimo(usuario, livro);
+
+        expect(saida).toBe(false);
+    });
+
+    test.each(casos)('$descricao', (caso) => {
+        const usuario = new Usuario({
+            id: 1,
+            nome: "Teste",
+            ativo: caso.ativo,
+            emprestimosAtivos: caso.emprestimosAtivos,
+            multaPendente: caso.multaPendente,
+        });
+        const livro = new Livro({
+            id: 1,
+            titulo: "Chapeuzinho Vermelho",
+            disponivel: caso.livroDisponivel,
+        });
+
+        if (caso.livroDisponivel) {
+            const saida = EmprestimoService.autorizarEmprestimo(usuario, livro);
+            expect(caso.saida).toBe(saida);
+        }
+        expect(() => EmprestimoService.autorizarEmprestimo(usuario, livro)).toThrow(
+            "Livro indisponível",
+        );
+    });
 });
--- javascript/src/service/EmprestimoService.js
+++ javascript/src/service/EmprestimoService.js
@@ -2,13 +2,18 @@ const { constantes } = require("../utils/constants");
 
 class EmprestimoService {
     static autorizarEmprestimo(usuario, livro) {
-        return this.validarUsuario(usuario);
+        return this.validarUsuario(usuario) && this.validarLivro(livro);
     }
 
     static validarUsuario(usuario) {
         if (!usuario.ativo) return false;
         if (usuario.emprestimosAtivos >= constantes.USUARIO_LIMITE_EMPRESTIMOS) return false;
         if (usuario.multaPendente >= constantes.USUARIO_LIMITE_MULTA) return false;
+        return true;
+    }
+
+    static validarLivro(livro) {
+        if (!livro.disponivel) throw new Error("Livro indisponível");
         return true;
     }
 }
